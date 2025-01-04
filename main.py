857961132772cdc4d04b86dab6830764c85045f8
Soulucid <soulucid.ai@gmail.com>
2025-01-04 19:04:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,13 @@ if __name__ == "__main__":
             try:
                 return recog.recognize_google(audio)
             except sr.RequestError:
+                print(f"User: {text}")
                 speak("Sorry, I'm having trouble with my speech recognition.")
                 exit()
             except Exception as e:
                 while True:
                     speak("Sorry, I didn't catch that. Can you repeat?")
+                    print(f"User: {text}")
                     with sr.Microphone() as source:
                           recog.adjust_for_ambient_noise(source)
                           audio = recog.listen(source)
@@ -42,6 +44,7 @@ if __name__ == "__main__":
 while True:
     speak("What can I do for you?")
     text = listen().lower()
+    print(f"User: {text}")
     if "search" in text:
         speak("What do you want me to search for?")
         search = listen().lower()
@@ -54,13 +57,13 @@ while True:
             break
     elif "hello" in text:
         speak("Hello! I think I am working properly.")
-    elif "how are you" in text:
+    elif "how are you" or "how r u" in text:
         speak("I'm doing well. Thank you for asking.")
     elif "what is your name" in text:
         speak("My name is Jarvis.")
     elif "what can you do" in text:
-            speak("I can search the internet for you, and more")
-    elif "who made you" in text:
+            speak("I can search the internet for you. I can also tell you the time and the weather.")
+    elif "who made you" or "who made u" in text:
         speak("I was made by a person named Soulucid. His information is available at https://guns.lol/soulucid/")
     elif "what is the time" in text:
         speak(time.ctime())
@@ -71,7 +74,7 @@ while True:
         open = webbrowser.open(url)
     elif "what is the news" in text:
         speak("I'll search it up on the internet for you.")
-        url = f"https://search.brave.com/search?q={text}"
+        url = f"https://search.brave.com/search?q=Latest news near me"
         response = requests.get(url)
         open = webbrowser.open(url)
     else:
